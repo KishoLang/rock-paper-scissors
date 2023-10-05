6efbd1eff17a00515e8d40c33e45e24c721f4c8f
KishoLang <aklangenberg@gmail.com>
2023-10-05 18:45:02 +0200

    finished rps

--- play.js
+++ play.js
@@ -12,7 +12,14 @@ const scissorsBtn = document.getElementById("scissors");
 const playerScoreText = document.getElementById("player-score");
 const computerScoreText = document.getElementById("computer-score");
 const roundText = document.getElementById("round-num");
+const result = document.getElementById("result");
 
+// Grab reference to dialog modal window
+const modal = document.getElementById("modal");
+const modalText = document.getElementById("modal-winner-text");
+const modalBtn = document.getElementById("modal-btn");
+
+// Check for Button presses
 rockBtn.addEventListener("click", () => {
     playOneRound("Rock");
     window.setTimeout(handleRounds, 500);
@@ -26,6 +33,10 @@ paperBtn.addEventListener("click", () => {
 scissorsBtn.addEventListener("click", () => {
     playOneRound("Scissors");
     window.setTimeout(handleRounds, 500);
+})
+
+modalBtn.addEventListener("click", () => {
+    newGame();
 })
 
 function removeFocus() {
@@ -45,17 +56,34 @@ function handleRounds() {
     }
 }
 
+function newGame() {
+    modal.close();
+    round = 1;
+    roundText.textContent = round;
+    playerScore = 0;
+    playerScoreText.textContent = playerScore;
+    computerScore = 0;
+    computerScoreText.textContent = computerScore;
+    result.textContent = "Rock...Paper...Scissors!";
+    rockBtn.disabled = false;
+    paperBtn.disabled = false;
+    scissorsBtn.disabled = false;
+}
+
 function playOneRound(playerChoice) {
     determineWinner(playerChoice, getComputerChoice());
 } 
 
 function wonBestOfFive() {
     if (playerScore > computerScore) {
-        console.log(`You have won with ${playerScore} to ${computerScore}!`);
+        modalText.textContent = `You have won with ${playerScore} to ${computerScore}!`;
+        modal.showModal();
     } else if (playerScore < computerScore) {
-        console.log(`You have lost with ${playerScore} to ${computerScore}!`);
+        modalText.textContent = `You have lost with ${playerScore} to ${computerScore}!`;
+        modal.showModal();
     } else if (playerScore == computerScore) {
-        console.log(`It's a draw! You both have ${playerScore} points.`);
+        modalText.textContent = `It's a draw! You both have ${playerScore} points.`;
+        modal.showModal();
     }
 }
 
@@ -79,39 +107,39 @@ function getComputerChoice() {
 function determineWinner(player, computer) {
     if (player === "Rock") {
         if (computer === "Rock") {
-            console.log("It's a draw! You both had Rock.");
+            result.textContent = "It's a draw! You both had Rock.";
         } else if (computer === "Paper") {
-            console.log("You lose! Paper beats Rock");
+            result.textContent = "You lose! Paper beats Rock";
             computerScore++;
             computerScoreText.textContent = computerScore;
         } else if (computer === "Scissors") {
-            console.log("You win! Rock beats Scissors.");
+            result.textContent = "You win! Rock beats Scissors.";
             playerScore++;
             playerScoreText.textContent = playerScore;
         }
     } else if (player === "Paper") {
         if (computer === "Rock") {
-            console.log("You win! Paper beats Rock");
+            result.textContent = "You win! Paper beats Rock";
             playerScore++;
             playerScoreText.textContent = playerScore;
         } else if (computer === "Paper") {
-            console.log("It's a draw! You both had Paper.");
+            result.textContent = "It's a draw! You both had Paper.";
         } else if (computer === "Scissors") {
-            console.log("You lose! Scissors beat Paper.");
+            result.textContent = "You lose! Scissors beat Paper.";
             computerScore++;
             computerScoreText.textContent = computerScore;
         }
     } else if (player === "Scissors") {
         if (computer === "Rock") {
-            console.log("You lose! Rock beats Scissors.");
+            result.textContent = "You lose! Rock beats Scissors.";
             computerScore++;
             computerScoreText.textContent = computerScore;
         } else if (computer === "Paper") {
-            console.log("You win! Scissors beat Paper.");
+            result.textContent = "You win! Scissors beat Paper.";
             playerScore++;
             playerScoreText.textContent = playerScore;
         } else if (computer === "Scissors") {
-            console.log("It's a draw! You both had Scissors");
+            result.textContent = "It's a draw! You both had Scissors";
         }
     }
 }
